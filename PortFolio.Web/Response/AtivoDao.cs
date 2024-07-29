944422ab83ae3ad2d474cc966c6eb6463902bea0
Ricardo Moreira <ricardo.moreira@g.globo>
2024-07-29 15:42:45 -0300

    remove old app

--- PortFolio.Web/Response/AtivoDao.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PortFolio.Web.Response
-{
-    public class AtivoDao
-    {
-        public string Codigo { get; set; }
-        public string Nome { get; set; }
-        public string Tipo { get; set; }
-    }
-}
